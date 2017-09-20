f2aea27462f1d6ca7a96f42d7227e3b108ad0cee
Ivan Magda <imagda15@gmail.com>
2017-09-21 00:05:31 +0300

    chore: Convert to Swift 3 #1

--- Countdowner WatchKit Extension/DateHelper.swift
+++ Countdowner WatchKit Extension/DateHelper.swift
@@ -1,10 +1,24 @@
-//
-//  DateHelper.swift
-//  Countdowner
-//
-//  Created by Ivan Magda on 12/07/16.
-//  Copyright © 2016 Ivan Magda. All rights reserved.
-//
+/**
+ * Copyright (c) 2017 Ivan Magda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import Foundation
 import SwiftDate
@@ -12,27 +26,27 @@ import SwiftDate
 // MARK: DateHelper
 
 final class DateHelper {
-    
-    // MARK: Init
-    
-    private init() {}
-    
-    // MARK: Behavior
- 
-    class func startOfTomorrow() -> NSDate {
-        return (NSDate() + 1.days).startOf(.Day)
-    }
-    
-    class func startOfNextWeek() -> NSDate {
-        return (NSDate() + 1.weeks).startOf(.Day)
-    }
-    
-    class func startOfNextMonth() -> NSDate {
-        return (NSDate() + 1.months).startOf(.Day)
-    }
-    
-    class func startOfNextYear() -> NSDate {
-        return (NSDate() + 1.years).startOf(.Day)
-    }
-    
+  
+  // MARK: Init
+  
+  private init() {}
+  
+  // MARK: - Behavior -
+  
+  class func startOfTomorrow() -> Date {
+    return (Date() + 1.days).startOf(component: .day)
+  }
+  
+  class func startOfNextWeek() -> Date {
+    return (Date() + 1.weeks).startOf(component: .day)
+  }
+  
+  class func startOfNextMonth() -> Date {
+    return (Date() + 1.months).startOf(component: .day)
+  }
+  
+  class func startOfNextYear() -> Date {
+    return (Date() + 1.years).startOf(component: .day)
+  }
+  
 }
--- Countdowner WatchKit Extension/TomorrowInterfaceController.swift
+++ Countdowner WatchKit Extension/TomorrowInterfaceController.swift
@@ -1,10 +1,24 @@
-//
-//  InterfaceController.swift
-//  Countdowner WatchKit Extension
-//
-//  Created by Ivan Magda on 12/07/16.
-//  Copyright © 2016 Ivan Magda. All rights reserved.
-//
+/**
+ * Copyright (c) 2017 Ivan Magda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import WatchKit
 import Foundation
@@ -12,17 +26,18 @@ import Foundation
 // MARK: TomorrowInterfaceController: WKInterfaceController
 
 class TomorrowInterfaceController: WKInterfaceController {
-
-    // MARK: Outlets
+  
+  // MARK: Outlets
+  
+  @IBOutlet var timer: WKInterfaceTimer!
+  
+  // MARK: Life Cycle
+  
+  override func willActivate() {
+    super.willActivate()
     
-    @IBOutlet var timer: WKInterfaceTimer!
-    
-    // MARK: Life Cycle
-
-    override func willActivate() {
-        super.willActivate()
-        timer.setDate(DateHelper.startOfTomorrow())
-        timer.start()
-    }
-
+    timer.setDate(DateHelper.startOfTomorrow())
+    timer.start()
+  }
+  
 }
